e88ca22a041bab61a6cb96ed3c3cd71c5f624974
Oleksii Matiiasevych <lastperson@gmail.com>
2015-03-15 17:59:08 +0200

    second

--- src/shcherbakov/Main.java
+++ src/shcherbakov/Main.java
@@ -8,12 +8,16 @@ import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -26,12 +30,11 @@ public class Main {
 		
 		TestHelper.findElement("//a[text()='load']").click();
 		
-		TestHelper.assertStringInFile("C:/Users/Oleksii/Downloads/file.txt", "<We are looking hasd for this string>");
+		TestHelper.assertStringInFile("C:/Users/Oleksii/Downloads/file.txt", "<We are looking for this string>");
 		//StringSelection ss = new StringSelection("C:\\MyWork\\AUT5\\newRepo\\.gitignore");
         //Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
 		
 		//Robot robot = new Robot();
-
 
 		//robot.keyPress(KeyEvent.VK_ENTER);
         //robot.keyRelease(KeyEvent.VK_ENTER);
@@ -40,7 +43,10 @@ public class Main {
 	}
 	
 	@After
-	public void after() {
-		TestHelper.Browser().quit();
+	public void after() throws IOException {
+		File scrFile = ((TakesScreenshot) TestHelper.Browser()).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("c:/tmp/Thistest.png"));
+		FileUtils.deleteQuietly(new File("C:/Users/Oleksii/Downloads/file.txt"));
+        TestHelper.Browser().quit();
 	}
 }
